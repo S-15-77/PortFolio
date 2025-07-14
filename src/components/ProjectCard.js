4ba7058561ef8f22a8772ebcbf55ea19faf1fbc6
Santhosh D <santhoshdayakar2002@gmail.com>
2025-07-13 20:46:29 -0400

    Fix class attribute in ProjectCard component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,7 +7,7 @@ function ProjectCard({ image,title,description,tech,link }) {
             <h2>{title}</h2>
             <div className="description">{description}</div>
             <div className="tech">{tech}</div>
-            <a href={link} target="_blank" rel='Open in a new tab'>View Project <span class="arrow">➜</span> </a>
+            <a href={link} target="_blank" rel='Open in a new tab'>View Project <span className="arrow">➜</span> </a>
         </div>
     );
 }
